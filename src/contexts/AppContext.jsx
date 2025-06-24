617035aa6d83afc96ccdad0d1f1b4e1ad48e5a1b
Jonathan <villagomezhjonathan@gmail.com>
2025-06-23 22:38:12 -0400

    added add triage function

--- src/contexts/AppContext.jsx
+++ src/contexts/AppContext.jsx
@@ -1,1 +1,29 @@
-import { createContext } from 'react'
+import { createContext, useState, useEffect } from 'react'
+import { setStorageItem, getStorageItem } from '../utils/localStorage'
+
+export const AppContext = createContext()
+
+export const AppProvider = (props) => {
+  const [triages, setTriages] = useState([])
+
+  const addTriage = (newTriage) => {
+    const updatedTriages = [...triages, newTriage]
+    
+    setTriages(updatedTriages)
+    setStorageItem('triages', updatedTriages)
+  }
+
+  useEffect(() => {
+    const storageTriages = getStorageItem('triages')
+    if (storageTriages) setTriages(storageTriages)
+  }, [])
+  
+  return (
+    <AppContext.Provider value={{
+      triages, 
+      addTriage
+    }}>
+      {props.children}
+    </AppContext.Provider>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import './styles/index.css'
+import { AppProvider } from './contexts/AppContext.jsx'
 import App from './App.jsx'
+import './styles/index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <AppProvider>
+        <App />
+      </AppProvider>
     </BrowserRouter>
   </StrictMode>,
 )
