55edda5e5677b111c4b5c757a7e2a13ff48bcd88
Jonathan <villagomezhjonathan@gmail.com>
2025-06-23 23:17:23 -0400

    worked on new triage functionality

--- src/components/routes/NewTriage/NewTriage.jsx
+++ src/components/routes/NewTriage/NewTriage.jsx
@@ -1,23 +1,42 @@
-import { useState } from 'react'
+import { useContext, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { AppContext } from '../../../contexts/AppContext'
 import './NewTriage.css'
 
 const NewTriage = () => {
-  const [formState, setFormState] = useState({
+  const appContext = useContext(AppContext)
+  const navigate = useNavigate()
+  
+  const initialFormState = {
     name: ''
-  })
+  }
+  const [formState, setFormState] = useState(initialFormState)
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault()
+
+    appContext.addTriage(formState)
+
+    setFormState(initialFormState)
+
+    navigate('/')
+  }
 
   const handleChange = (evt) => {
-    console.log(evt)
+    const target = evt.target
+    const newFormState = {...formState, [target.name]: target.value}
+    setFormState(newFormState)
   }
   
   return (
     <div className='NewTriage'>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor='name'>Name</label>
         <input 
           type='text'
           name='name'
           id='name'
+          value={formState.name}
           onChange={(evt) => handleChange(evt)}
         />
 
