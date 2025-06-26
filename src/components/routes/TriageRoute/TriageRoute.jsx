376a813028a84a92b1f704c4f14ef6040820c3f7
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2025-06-25 23:57:35 -0400

    removed sub bar on unmount

--- src/components/routes/TriageRoute/TriageRoute.jsx
+++ src/components/routes/TriageRoute/TriageRoute.jsx
@@ -55,6 +55,10 @@ const TriageRoute = () => {
   useEffect(() => {
     populateTriage()
     addSubBar()
+
+    return () => {
+      layoutContext.addSubBar(null)
+    }
   }, [appContext])
 
   return (
