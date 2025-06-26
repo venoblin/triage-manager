945f5355fd3cceb945e697a0c2e2fda0d9a03e37
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2025-06-26 00:24:28 -0400

    added expand functionality

--- src/components/DeviceCard/DeviceCard.jsx
+++ src/components/DeviceCard/DeviceCard.jsx
@@ -1,20 +1,32 @@
+import { useState } from 'react'
 import DevicePaths from '../DevicePaths/DevicePaths'
 import './DeviceCard.css'
 
 const DeviceCard = (props) => {
+  const [isExpanded, setIsExpanded] = useState(false)
 
   const selectDevice = () => {
     props.setSelectedDevice(props.device)
   }
 
+  const toggleExpanded = () => {
+    setIsExpanded((currState) => !currState)
+  }
+
   return (
     <div className={`DeviceCard ${props.selectedDevice && props.selectedDevice.id === props.device.id ? 'selected' : ''}`}>
+      <button onClick={toggleExpanded}>Expand</button>
+      
       <h2>{props.device.hostName}</h2>
 
       <button onClick={selectDevice}>Edit</button>
 
       {props.device.paths.length ? (
-        <DevicePaths device={props.device} />
+        isExpanded ? (
+          <DevicePaths device={props.device} />
+        ) : (
+          <p className='count'>{`${props.device.paths.length} paths`}</p>
+        )
       ) : (
         <p>There are no paths!</p>
       )}
