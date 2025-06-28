aa164035120bac9644cb93d4db775375ffa161ff
Jonathan <villagomezhjonathan@gmail.com>
2025-06-27 22:05:45 -0400

    added input to device selector

--- src/components/routes/TriageRoute/TriageRoute.jsx
+++ src/components/routes/TriageRoute/TriageRoute.jsx
@@ -7,58 +7,16 @@ import './TriageRoute.css'
 
 const TriageRoute = () => {
   const appContext = useContext(AppContext)
-  const layoutContext = useContext(LayoutContext)
   const {id} = useParams()
   
-  const initialFormState = {
-    hostName: ''
-  }
-  const [formState, setFormState] = useState(initialFormState)
   const [triage, setTriage] = useState()
 
-  const handleSubmit = (evt) => {
-    evt.preventDefault()
-
-    appContext.postDevice(triage.id, formState)
-
-    setFormState(initialFormState)
-  }
-
-  const handleChange = (evt) => {
-    const target = evt.target
-    const newFormState = {...formState, [target.name]: target.value}
-    setFormState(newFormState)
-  }
-  
   const populateTriage = () => {
     setTriage(appContext.getTriage(id))
   }
 
-  const addSubBar = () => {
-    layoutContext.addSubBar(
-      <div className='triage-head'>
-        <form onSubmit={handleSubmit}>
-          <label htmlFor='hostName'>Hostname</label>
-          <input 
-            type='text'
-            name='hostName'
-            id='hostName'
-            value={formState.hostName}
-            onChange={(evt) => handleChange(evt)}
-          />
-          <button>Add Device</button>
-        </form>
-      </div>
-    )
-  }
-
   useEffect(() => {
     populateTriage()
-    addSubBar()
-
-    return () => {
-      layoutContext.addSubBar(null)
-    }
   }, [appContext])
 
   return (
